30e53dac515454466a81b45711f007e3958bae40
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-01-24 21:54:46 +0500

    first

---
+++ FlagViewer/DetailViewController.swift
@@ -0,0 +1,77 @@
+//
+//  DetailViewController.swift
+//  FlagViewer
+//
+//  Created by Zeeshan Waheed on 24/01/2024.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet var ImageView: UIImageView!
+    
+    var pictures_hd = [String]()
+    var selectedImage: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let flags = try! fm.contentsOfDirectory(atPath: path)
+        
+        for flag in flags {
+            if flag.hasSuffix(".png") {
+                pictures_hd.append(flag)
+            }
+        }
+        
+        //        let sorted_pictures = pictures_hd.sorted()
+        
+        //        var num: Int = 0
+        
+        func namingFlag() -> String {
+            for name in pictures_hd {
+                if name == selectedImage {
+                    return name
+                }
+            }
+            return ""
+        }
+        
+        title = namingFlag()
+        //        navigationItem.largeTitleDisplayMode = .never
+        
+        
+        print("This is hd: ", pictures_hd)
+        
+        if let imageToLoad = selectedImage {
+            ImageView.image = UIImage(named: imageToLoad)
+        }
+        
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    
+    @objc func shareTapped() {
+        let items: [Any] = ["\(selectedImage!)"]
+        let vc = UIActivityViewController(activityItems: items, applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
--- FlagViewer/ViewController.swift
+++ FlagViewer/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var pictures_hd = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Flag Viewer"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let flags = try! fm.contentsOfDirectory(atPath: path)
+        
+        for flag in flags {
+            if flag.hasSuffix(".png") {
+                pictures_hd.append(flag)
+            }
+        }
+        
+//        for flag in flags {
+//            if flag.hasPrefix("flag_sd") {
+//                pictures_sd.append(flag)
+//            }
+//        }
+        
+        pictures_hd.sort()
+//        pictures_sd.sort()
+//        
+        print("This is hd: ", pictures_hd)
+//        print()
+//        print("This is sd: ", pictures_sd)
+        
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures_hd.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row+1).  " + pictures_hd[indexPath.row].replacingOccurrences(of: ".png", with: "")
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier:"Detail") as? DetailViewController {
+            let sortedPictures = pictures_hd.sorted()
+            vc.selectedImage = sortedPictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
