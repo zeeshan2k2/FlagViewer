20cf65befc04d7272275088202bdff323ae59d5b
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-01-25 15:33:32 +0500

    share button

--- FlagViewer/DetailViewController.swift
+++ FlagViewer/DetailViewController.swift
@@ -53,8 +53,7 @@ class DetailViewController: UIViewController {
             ImageView.image = UIImage(named: imageToLoad)
         }
         
-        
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareFlag))
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -68,9 +67,22 @@ class DetailViewController: UIViewController {
     }
     
     
-    @objc func shareTapped() {
-        let items: [Any] = ["\(selectedImage!)"]
-        let vc = UIActivityViewController(activityItems: items, applicationActivities: [])
+//    @objc func shareFlag() {
+//        let items: [Any] = ["\(selectedImage!)"]
+//        let vc = UIActivityViewController(activityItems: items, applicationActivities: [])
+//        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+//        present(vc, animated: true)
+//    }
+    
+    @objc func shareFlag() {
+        guard let image = ImageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+//      this shares two things an image and a name the commented line below only shares an image
+        let vc = UIActivityViewController(activityItems: [title ?? "Unknown country", image], applicationActivities: [])
+//        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
